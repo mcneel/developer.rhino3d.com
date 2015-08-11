d89240e58c3c5bde98f31d54e5697d9a603521ae
Will Pearson <pearswj@gmail.com>
2015-08-11 23:35:54 +0100

    more using directives and wrap sample in a class

--- test/compile-sample.rb
+++ test/compile-sample.rb
@@ -43,16 +43,26 @@ files.each do |filename|
   preamble << 'using System.Threading;'
   preamble << 'using System.Threading.Tasks;'
   preamble << 'using Rhino;'
+  preamble << 'using Rhino.Collections;'
   preamble << 'using Rhino.Commands;'
   preamble << 'using Rhino.DocObjects;'
+  preamble << 'using Rhino.Display;'
   preamble << 'using Rhino.Geometry;'
+  preamble << 'using Rhino.Geometry.Collections;'
+  preamble << 'using Rhino.Geometry.Intersect;'
+  preamble << 'using Rhino.Geometry.Morphs;'
+  preamble << 'using Rhino.Input;'
   preamble << 'using Rhino.Input.Custom;'
+  preamble << 'using Rhino.UI;'
+  preamble << 'using Rhino.UI.Gumball;'
   preamble << ''
 
   # write to file
   File.open(filename + '.cs', 'w') do |file|
     preamble.each { |line| file.puts(line) }
+    file.puts("class CompileClass\n\{") # wrap sample in a class, in case it is not already
     code.each { |line| file.puts(line) }
+    file.puts("\}")
   end
 
   # compile against rhinocommon and throw a wobbly if there's an error
@@ -61,12 +71,15 @@ files.each do |filename|
   # > cp RhinoCommon*/lib/RhinoCommon.dll .
   output = `mcs #{filename}.cs -pkg:dotnet -r:RhinoCommon.dll -t:library 2>&1`
   unless $?.success?
+    # print output
     output.split("\n").each do |line|
       puts "    \033[37m%s\033[0m" % line # print line in grey with indent
     end
+    # TODO last line is unnecessary (e.g. Compilation failed: 2 error(s), 0 warnings)
     puts "\u2717".encode('utf-8') + ' Could not compile ' + filename
     state << false
   else
+    # TODO show warnings
     puts "\u2713".encode('utf-8') + ' Successfully compiled ' + filename
     state << true
   end
