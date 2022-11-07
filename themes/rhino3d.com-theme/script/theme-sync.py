fc0722ea45d3b0bed4a3a6ff989af6e63684d594
Dan Rigdon-Bel <dan@mcneel.com>
2022-11-07 14:42:14 -0800

    Sync theme

--- themes/rhino3d.com-theme/script/theme-sync.py
+++ themes/rhino3d.com-theme/script/theme-sync.py
@@ -14,6 +14,7 @@ from subprocess import Popen, PIPE
 import sys
 import os
 import shutil
+from datetime import datetime, timedelta
 
 # sites - sites must reside in folder that are parallel to each other. For example:
 # /dev/mcneel/rhino3d.com
@@ -31,6 +32,21 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+
+def get_last_modified_theme_list_for_site(site_root):
+    theme_path = os.path.abspath(os.path.join(site_root, "themes", "rhino3d.com-theme"))
+    all_files_in_theme = [os.path.join(dp, f) for dp, dn, filenames in os.walk(theme_path) for f in filenames]
+    last_modified_dictionary = {}
+    for file in all_files_in_theme:
+        encoding = 'utf-8'
+        p_git_last_updated = subprocess.Popen(['git', 'log', '-1', '--pretty=%ci', file], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        last_updated, err = p_git_last_updated.communicate()
+        last_updated = str(last_updated, encoding).rstrip()
+        file_truncated = 'themes/rhino3d.com-theme/' + file.split("/themes/rhino3d.com-theme/")[1]  # only use the relative theme paths as keys
+        last_modified_dictionary[file_truncated] = last_updated
+
+    return last_modified_dictionary
 
 
 def print_warning_message(warning_message):
@@ -77,11 +93,13 @@ def main():
         else:
             print_error_message("Could not find " + site + ". Did you clone it? It needs to be a folder parallel to that which you are working in.")
             sys.exit(1)
-
-
+    
     # Synchronize the rhino3d.com-theme
     changes_were_made = False
     print("Synchronizing theme in other sites...")
+    # Get a list of all the files in source theme and the date they last changed
+    source_theme_last_modified = get_last_modified_theme_list_for_site(site_root_folder_path)
+
     for site in sites:
         if running_in_site == site:
             continue
@@ -89,8 +107,21 @@ def main():
         target_site_path = os.path.abspath(os.path.join(root_folder_path, site))
         path_to_theme = os.path.abspath(os.path.join(target_site_path, "themes", "rhino3d.com-theme"))
 
-        # Check if there are chan ges in this folder - if so, bail
         os.chdir(target_site_path)
+
+        # Check to see if the remote origin has changes that need to be pulled
+        p_git_update = subprocess.Popen(['git', 'remote', 'update'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        git_status, err = p_git_update.communicate()
+        p_git_status = subprocess.Popen(['git', 'status', '-uno'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        git_status, err = p_git_status.communicate()
+        if not 'Your branch is up to date with' in str(git_status):
+            print_warning_message(site + " is not up-to-date with origin. Skipping.")
+            continue
+
+        # Get a list of all the modified dates for all the files in the target...this is for comparison later
+        target_theme_last_modified = get_last_modified_theme_list_for_site(target_site_path)
+
+        # Check if there are changes in this folder - if so, bail
         p_git_status = subprocess.Popen(['git', 'status', 'themes/rhino3d.com-theme'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         git_status, err = p_git_status.communicate()
         if 'nothing to commit, working tree clean' in str(git_status):
@@ -129,11 +160,30 @@ def main():
         if 'nothing to commit, working tree clean' in str(git_status):
             print_ok_message(site + " was already in sync. No changes were made.")
         else:
-            print_ok_message(site + "'s theme/rhino3d.com theme updated.")
             changes_were_made = True
 
+        if changes_were_made:
+            # Check to make sure we are not overwriting newer changes from the other repos
+            os.chdir(target_site_path)
+            p_git_modified_files = subprocess.Popen(['git', 'ls-files', '-m'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            modified_files, err = p_git_modified_files.communicate()
+            encoding = 'utf-8'
+            modified_files = str(modified_files, encoding).split('\n')
+            modified_files = modified_files[:-1]
+
+            # get the last modified date of those files in the target repo and compare it to the source modified dates
+            for modified_file in modified_files:
+                source_file_last_modified = source_theme_last_modified[modified_file]
+                target_file_last_modified = target_theme_last_modified[modified_file]
+                source_file_last_modified_date = datetime.strptime(source_file_last_modified, '%Y-%m-%d %H:%M:%S %z')
+                target_file_last_modified_date = datetime.strptime(target_file_last_modified, '%Y-%m-%d %H:%M:%S %z')
+                if target_file_last_modified_date > source_file_last_modified_date:
+                    print_warning_message(modified_file + ' has been edited in the ' + site + '\'s theme more recently.')
+
+            print_ok_message(site + "'s theme/rhino3d.com theme updated.")
+
     if changes_were_made:
-        print_warning_message("DONE: theme synchronization finished without commiting changes. Please review changes and commit/push or revert the changes.")        
+        print_warning_message("DONE: theme synchronization finished without commiting changes. Please review changes and commit/push or revert the changes.")
     else:
         print("DONE: theme synchronization done. No changes were made (themes were already in sync or skipped)")
 
